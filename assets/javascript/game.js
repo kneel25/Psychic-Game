2ec0f1afe6845db6c1cd11131f9e51a597dce4f4
Krista Neel <krista@Kristas-MacBook-Air.local>
2018-03-12 19:40:30 -0500

    homework#3

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,42 +1,42 @@
-//start typing - js code. -kn 
+//just start typing JS.
 
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
 
 	var wins = 0;
     var losses = 0;
-	var numGuesses = 9;
+	var numberGuesses = 9;
 	var guessChoices = [];
 
-		document.onkeyup = function(event) {
-		var userGuess = event.key;
-		var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-	    var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+	document.onkeyup = function(event) {
+	var userGuess = event.key;
+	var compGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+	var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
 		 	
 		if (options.indexOf(userGuess) > -1) {
-			if (userGuess === computerGuess) {
+			if (userGuess === compGuess) {
 				wins++;
-				numGuesses = 9;
+				numberGuesses = 9;
 				guessChoices = [];
 			}
-			if (userGuess != computerGuess) {
-				numGuesses --;
+			if (userGuess != compGuess) {
+				numberGuesses --;
 				guessChoices.push(userGuess);
 			}
-			if (numGuesses === 0) {
-			    numGuesses = 9;
+			if (numberGuesses === 0) {
+			    numberGuesses = 9;
 			    losses ++;
 			    guessChoices = [];
 			}
 
-		var html = 
-		"<h1> The Psychic Game </h1>" +
-		"<p>Guess what letter I'm thinking of!</p>" +
-		"<p>Wins: " + wins + "</p>" +
-		"<p>Losses: " + losses + "</p>" +
-		"<p>Guesses Left: " + numGuesses + "</p>" +
-        "<p>Your Guesses so far: " + guessChoices.join(", ") + "</p>";
+	var html = 
+	"<h1> Zoltar's Psychic Game </h1>" +
+	"<p>Can You Read My Mind??!</p>" +
+	"<p>Winner!: " + wins + "</p>" +
+	"<p>Loser!: " + losses + "</p>" +
+	"<p>Letters Left: " + numberGuesses + "</p>" +
+    "<p>Letters Guessed: " + guessChoices.join(", ") + "</p>";
             
-		document.querySelector("#game").innerHTML = html;
+	document.querySelector("#game").innerHTML = html;
 
 			
 		}
